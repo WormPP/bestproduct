bd6be4ab3d9eaa4854e5de4f8e9f00033c3a8251
WormPP <51852417+WormPP@users.noreply.github.com>
2021-03-07 22:37:54 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$default_main_site = 'http://api.cpa.tl';
+$landings_default_domain = 'wormpp.github.io/bestproduct';
+$shared_path = '/bestproduct';
+
+$dispathchUrl = $default_main_site . '/api/tds/r/VrxFR82V/s';
+$apiUrl = $default_main_site . '/api/lead/send_archive';
+
+$apiKey = 'qLA1wdlRixaTtbAYNMa2thKOagwC77HREpHZSaigIp7CWz1H1JwKGga7aVEA';
+$offer_id = 1135;
+$stream_hid = 'VrxFR82V';
+$landKey = '{land_key}';
+
+$_debug = False; // установите True для вывода дополнительной информации для отладки и поиска ошибок
